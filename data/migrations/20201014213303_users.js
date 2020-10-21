f185a10850af9ed132cdd51ccd9e26fdb058c3f6
spazdrum <spazdrum@gmail.com>
2020-10-20 21:30:06 -0400

    added tracks and favorite songs to table

--- data/migrations/20201014213303_users.js
+++ data/migrations/20201014213303_users.js
@@ -1,11 +1,53 @@
 exports.up = function (knex, Promise) {
-  return knex.schema.createTable("users", (user) => {
-    user.increments();
-    user.text("username", 255).unique().notNullable();
-    user.text("password", 255).notNullable();
-  });
+  return knex.schema
+    .createTable("users", (user) => {
+      user.increments();
+      user.text("username", 255).unique().notNullable();
+      user.text("password", 255).notNullable();
+    })
+    .createTable("tracks", (tbl) => {
+      tbl.increments();
+      tbl.string("track_id").notNullable();
+      tbl.string("track_name").notNullable();
+      tbl.string("artist_name").notNullable();
+      tbl.float("acousticness").unsigned();
+      tbl.integer("duration_ms").unsigned();
+      tbl.float("energy").unsigned();
+      tbl.float("instrumentalness").unsigned();
+      tbl.integer("key").unsigned();
+      tbl.float("liveness").unsigned();
+      tbl.float("loudness");
+      tbl.integer("mode").unsigned();
+      tbl.float("speechiness").unsigned();
+      tbl.float("tempo");
+      tbl.integer("time_signature").unsigned();
+      tbl.float("valence").unsigned();
+      tbl.integer("popularity");
+      tbl.string("genre");
+    })
+    .createTable("favoriteSongs", (tbl) => {
+      tbl.increments();
+      tbl
+        .integer("user_id")
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .string("song_id")
+        .notNullable()
+        .references("id")
+        .inTable("tracks")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl.string("full_track_id").notNullable();
+    });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("users");
+  return knex.schema
+    .dropTableIfExists("favoriteSongs")
+    .dropTableIfExists("tracks")
+    .dropTableIfExists("users");
 };
