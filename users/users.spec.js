bf88f6e59f3ede570836ba0bc2f438c2a1fa04d7
spazdrum <spazdrum@gmail.com>
2020-10-23 21:41:00 -0400

    added users testing

---
+++ users/users.spec.js
@@ -0,0 +1,22 @@
+const request = require("supertest");
+const server = require("../api/server");
+const db = require("./users-model");
+
+describe("server.js", () => {
+  describe("Grab all users", () => {
+    it("Should return status 200", async () => {
+      const res = await request(server).get("/");
+      expect(res.status).toBe(200);
+    });
+    it("Should return text/html", async () => {
+      const res = await request(server).get("/");
+      expect(res.type).toBe("text/html");
+    });
+  });
+  describe("Grab users by ID", () => {
+    it("Should grab users by ID", async () => {
+      const res = await request(server).get("/api/users/1");
+      expect(res.body.id).toBe(1);
+    });
+  });
+});
